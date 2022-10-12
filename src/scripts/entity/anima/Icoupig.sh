c5429a11b2c881811b6e6f4ed17ecc28247362cc
coppercu <coppercu@qq.com>
2022-10-12 23:40:03 +0800

    skeleton

--- src/scripts/entity/anima/Icoupig.sh
+++ src/scripts/entity/anima/Icoupig.sh
@@ -30,13 +30,45 @@ rel_path=${pwd_path##${imx_path}/}
 
 # 项目配置
 menuconfig Icoupig
+# 将.config更到icoupig.h
+mv .config icoupig.h
 
 # 主节点配置
-if [ -f Iconfig_master ]; then
-    menuconfig Iconfig_master
+if [ -f Iconfig ]; then
+    menuconfig Iconfig
+    if [ -f .config ]; then
+        mv .config iconfig.h
+    else
+        read -p "未产生.config" key
+        exit
+    fi
+else
+    # 从coupig镜像一份配置
+    cp icoupig.h iconfig.h
+    sed -i "s/icoupig/iconfig" iconfig.h
+    sed -i "s/ICOUPIG/ICONFIG" iconfig.h
+    read -p "未产生.config" key
+    exit
 fi
 
 # 从节点配置
 if [ -f Iconfig_assist ]; then
     menuconfig Iconfig_assist
+    if [ -f .config ] && [ -f ../../imodel.json ]; then
+        assist=`cat ../../imodel.json | jq .assist`
+        if [ -n $assist ]; then
+            if [ -d ../../../../../$assist/entity/$imodel/$iassem/$entity/ ]; then
+                cp .config ../../../../../$assist/entity/$imodel/$iassem/$entity/iconfig.h
+            fi
+        fi
+    fi
+fi
+
+if [ -f $imx_path/src/skindk/$master/entity/$imodel/$iassem/$entity/entity.json ]; then
+   iclass=`cat $imx_path/src/skindk/$master/entity/$imodel/$iassem/$entity/entity.json | jq .iclass`
+   iorder=`cat $imx_path/src/skindk/$master/entity/$imodel/$iassem/$entity/entity.json | jq .iorder`
+fi
+if [ -z $iclass or -z $iorder ]; then
+    read -p "iclass $iclas iorder $iorder 不达标" key
+    exit
 fi
